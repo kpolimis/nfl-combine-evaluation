31ea7addbc977a930afe50cb3ded427fe3495ef9
Rich Lee <richlee@outlook.com>
2016-02-17 19:13:27 -0800

    Uploading 2009-2015 statistics and code used to generate data

---
+++ data/import2009_2015data.py
@@ -0,0 +1,9 @@
+import nflgame
+
+nflgame.combine(nflgame.games(2009)).csv('season2009.csv')
+nflgame.combine(nflgame.games(2010)).csv('season2010.csv')
+nflgame.combine(nflgame.games(2011)).csv('season2011.csv')
+nflgame.combine(nflgame.games(2012)).csv('season2012.csv')
+nflgame.combine(nflgame.games(2013)).csv('season2013.csv')
+nflgame.combine(nflgame.games(2014)).csv('season2014.csv')
+nflgame.combine(nflgame.games(2015)).csv('season2015.csv')
